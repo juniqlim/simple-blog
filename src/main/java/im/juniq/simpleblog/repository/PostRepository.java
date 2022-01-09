87e8bc6aa9e836bce7829913b6036a8bcc205e4f
juniq <juniqlim@gmail.com>
2022-01-09 14:15:01 +0900

    repository를 이용하는 service 만들기

--- src/main/java/im/juniq/simpleblog/repository/PostRepository.java
+++ src/main/java/im/juniq/simpleblog/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package im.juniq.simpleblog.repository;
 
 import im.juniq.simpleblog.model.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByCategoryId(Long categoryId);
 }
