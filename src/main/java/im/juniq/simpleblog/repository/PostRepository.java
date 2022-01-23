b3f8953c8a974d0704dceec0d36f711157250a57
juniq <juniqlim@gmail.com>
2022-01-23 14:41:31 +0900

    feat: service 만들기

--- src/main/java/im/juniq/simpleblog/repository/PostRepository.java
+++ src/main/java/im/juniq/simpleblog/repository/PostRepository.java
@@ -3,5 +3,8 @@ package im.juniq.simpleblog.repository;
 import im.juniq.simpleblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByCategoryId(Long categoryId);
 }
---
+++ src/main/java/im/juniq/simpleblog/service/BlogService.java
@@ -0,0 +1,29 @@
+package im.juniq.simpleblog.service;
+
+import im.juniq.simpleblog.model.Category;
+import im.juniq.simpleblog.model.Post;
+import im.juniq.simpleblog.repository.CategoryRepository;
+import im.juniq.simpleblog.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BlogService {
+    private final CategoryRepository categoryRepository;
+    private final PostRepository postRepository;
+
+    public List<Category> getCategoryAll() {
+        return categoryRepository.findAll();
+    }
+
+    public List<Post> getPostAll() {
+        return postRepository.findAll();
+    }
+
+    public List<Post> getPosts(Long categoryId) {
+        return postRepository.findByCategoryId(categoryId);
+    }
+}
