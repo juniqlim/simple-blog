20b161457850f2f6b2aeba7906d0789629b309d8
juniq <juniqlim@gmail.com>
2022-01-23 15:19:45 +0900

    feat: controller 추가

---
+++ src/main/java/im/juniq/simpleblog/controller/BlogController.java
@@ -0,0 +1,27 @@
+package im.juniq.simpleblog.controller;
+
+import im.juniq.simpleblog.model.Category;
+import im.juniq.simpleblog.model.Post;
+import im.juniq.simpleblog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BlogController {
+    private final BlogService blogService;
+
+    @GetMapping("/categorys")
+    public List<Category> getCategorys() {
+        return blogService.getCategoryAll();
+    }
+
+    @GetMapping("/category/{categoryId}/posts")
+    public List<Post> getPosts(@PathVariable Long categoryId) {
+        return blogService.getPosts(categoryId);
+    }
+}
